fe076f0435154c3f436e1258728a1ff4a999ed12
maxbittker <maxbittker@gmail.com>
2018-01-21 00:29:15 -0800

    more

--- scrape.js
+++ scrape.js
@@ -34,7 +34,7 @@ let c = "";
 // PriceAsc
 async function scrapePage(offset) {
   let data = await fetchData(
-    `https://www.hugedomains.com/cat/special-categories/?start=${offset}&domain_name=&catSearch=1&dot=all&anchor=all&highlightbg=1&length_start=&length_end=&price_from=&price_to=&maxrows=100&sort=`
+    `https://www.hugedomains.com/domain_search.cfm?start=${offset}&domain_name=&catSearch=96&dot=all&anchor=all&highlightbg=1&length_start=&length_end=&price_from=&price_to=&maxrows=100&sort=`
 );
 // `https://www.hugedomains.com/cat/arts/?start=${offset}&domain_name=&catSearch=22&dot=all&anchor=all&highlightbg=1&length_start=&length_end=&price_from=&price_to=&maxrows=100&sort=`
 // `https://www.hugedomains.com/domain_search.cfm?dot=all&anchor=all&highlightbg=1&maxrows=100&catsearch=0&sort=PriceAsc&oc=0&start=${offset}`
