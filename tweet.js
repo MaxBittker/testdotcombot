448009a1222ba6378a9c54f6e07ed2c531d0bcf8
maxbittker <maxbittker@gmail.com>
2018-01-20 22:42:17 -0800

    tweet.s

--- tweet.js
+++ tweet.js
@@ -1,18 +1,41 @@
-var Twit = require('twit')
+var Twit = require("twit");
+var fs = require("fs");
+let dat = JSON.parse(fs.readFileSync("out.json"));
+let seen = JSON.parse(fs.readFileSync("seen.json"));
+
+let keys = Object.keys(dat).filter(key=>seen[key]===undefined);
+
+function saveFile(data) {
+  let d = JSON.stringify(data);
+  fs.writeFileSync("seen.json", d);
+}
 
 var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
+  consumer_key: "...",
+  consumer_secret: "...",
+  access_token: "...",
+  access_token_secret: "...",
+  timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+});
 
-//
-//  tweet 'hello world!'
-//
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  console.log(data)
-})
+function neuterUrl(url) {
+  return url.replace(".", "․");
+}
 
+function formatTweet(key) {
+  let { url, category, price } = dat[key];
+  return `${neuterUrl(url)}   \n\n ${price}`;
+}
 
+console.log(seen.length + "/" + keys.length);
+let key = keys.pop();
+let status = formatTweet(key);
+console.log(status);
+
+T.post("statuses/update", { status }, function(err, data, response) {
+  console.log(err, data);
+});
+
+seen[key] = true;
+
+saveFile(seen);
